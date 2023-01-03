86b321ac8073c3efca1d69aa069e7081d0d95e9e
Andre Filipe Ferreira Miranda <andrferr@c2r5p7.hive.fi>
2023-01-03 17:51:05 +0200

    faster to top function fixed

--- srcs/sort_utils_2.c
+++ srcs/sort_utils_2.c
@@ -6,7 +6,7 @@
 /*   By: andrferr <andrferr@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 13:03:29 by andrferr          #+#    #+#             */
-/*   Updated: 2023/01/03 14:53:45 by andrferr         ###   ########.fr       */
+/*   Updated: 2023/01/03 17:50:02 by andrferr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ static void	handle_checkes(t_stack *stack,
 	{
 		if (stack->arr[i] >= portion->start && stack->arr[i] <= portion->end)
 		{
-			*up = stack->arr[i];
+			*down = stack->arr[i];
 			break ;
 		}
 		i++;
@@ -32,21 +32,37 @@ static void	handle_checkes(t_stack *stack,
 	{
 		if (stack->arr[i] >= portion->start && stack->arr[i] <= portion->end)
 		{
-			*down = stack->arr[i];
+			*up = stack->arr[i];
 			break ;
 		}
 		i--;
 	}
 }
 
+static	int	get_index(t_stack *stack, int up, int down)
+{
+	int	i;
+	int	j;
+
+	i = -1;
+	while (stack->arr[++i] != up)
+		;
+	j = -1;
+	while (stack->arr[++j] != down)
+		;
+	if (stack->top - i < j)
+		return (i);
+	else
+		return (j);
+}
+
 int	faster_to_top(t_stack *stack, t_portion *portion)
 {
 	int	up;
 	int	down;
+	int	index;
 
 	handle_checkes(stack, portion, &up, &down);
-	if (stack->top - up < down)
-		return (up);
-	else
-		return (down);
+	index = get_index(stack, up, down);
+	return (stack->arr[index]);
 }
